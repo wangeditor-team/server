eb960c2a5ed734ebe889712bcda898c450446ce3
王福朋 <wangfupeng1988@163.com>
2021-06-10 14:42:13 +0800

    chore: fix deploy

--- bin/rm.sh
+++ bin/rm.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
 # 定时删除上传的文件，用于 crontab 调用
 
-cd /app/upload-files # docker 容器 path
+cd /home/work/wangEditor-team/server/upload-files # 测试机的 path
 rm -rf *
-
-# cd /home/work/wangEditor-team/server/upload-files # 测试机的 path
-# rm -rf *
